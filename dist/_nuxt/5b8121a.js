f1a5955f93cef8efb6920052a1725e6482132774
Vitaly Perekutnev <v.perekutnev@b2b-center.ru>
2022-08-21 22:23:44 +0300

    master generate dist

---
+++ dist/_nuxt/5b8121a.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[24],{524:function(n,t,e){"use strict";e.r(t);var l=e(49),component=Object(l.a)({},(function(){return(0,this._self._c)("div",[this._v("настройки")])}),[],!1,null,null,null);t.default=component.exports}}]);
